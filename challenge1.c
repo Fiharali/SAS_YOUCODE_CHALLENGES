89cd0ca1f5116aaf37ea60ced9b13054f05389c4
fihar abdelali <fiharali0@gmail.com>
2023-09-13 15:00:46 +0100

    third  commit

--- challenge1.c
+++ challenge1.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello alii!");
-  return 0;
+int main()
+{
+    char nom[] = "";
+    char sexe[] = "";
+    char prenom[] = "";
+    int age, numero;
+    printf("entre votre nom ");
+    scanf("%s", &nom);
+    printf("entre votre prenom ");
+    scanf("%s", &prenom);
+    printf("entre votre sexe ");
+    scanf("%s", &sexe);
+    printf("entre votre age ");
+    scanf("%d", &age);
+    printf("entre votre numero ");
+    scanf("%d", &numero);
+    printf("votre nom est : %s \n  votre prenom est :  %s \n votre sexe est %s \n votre age est %d \n  votre numero %d \n ", nom,prenom,sexe,age,numero);
+    return 0;
 }
