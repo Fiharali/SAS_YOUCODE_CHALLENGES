988c4e50f1e13833451d74d7e5c13b1e9a091ab0
fihar abdelali <fiharali0@gmail.com>
2023-09-13 18:00:03 +0100

    9commit

--- challenge1.c
+++ challenge1.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    char nom[20], prenom[20], sexe[10];
-    int age, numero;
+    char nom[20], prenom[20], sexe[10] ,numero[10];
+    int age;
     printf("entre votre nom ");
     scanf("%s", &nom);
     printf("entre votre prenom ");
@@ -13,8 +13,8 @@ int main()
     printf("entre votre age ");
     scanf("%d", &age);
     printf("entre votre numero ");
-    scanf("%d", &numero);
-    printf("votre nom est : %s \n  votre prenom est :  %s \n votre sexe est %s \n votre age est %d \n  votre numero %d \n ", nom, prenom, sexe, age, numero);
+    scanf("%s", &numero);
+    printf("votre nom est : %s \n  votre prenom est :  %s \n votre sexe est %s \n votre age est %d \n  votre numero %s \n ", nom, prenom, sexe, age, numero);
 
     return 0;
 }
---
+++ challenge6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int r = 2;
+    scanf("%d", &r);
+    double CC = 2 * M_PI * r;
+
+    printf("La circonfrence du cercle est : %lf\n", CC);
+
+    return 0;
+}
