4d4cd286f9670e355b11858c7d35b8fb5a48410f
ali <fiharali0@gmail>
2023-09-14 21:30:50 +0200

    18commit

--- III. Les Boucles/challengesIII5.c
+++ III. Les Boucles/challengesIII5.c
@@ -3,8 +3,25 @@
 
 int main()
 {
+    int num, div, count = 0, mod, numInv;
+    printf("entre number ::   ");
+    scanf("%d", &num);
+    div = num;
+    while (div > 10)
+    {
+        div = div / 10;
+        count = count + 1;
+    }
 
-    
+    printf("%d", count + 1);
+
+    for (int i = 0; i < count + 1; i++)
+    {
+        mod = num % 10;
+        numInv = numInv * 10 + mod;
+        num = num / 10;
+    }
+    printf("L'inverse du numero est  %d ", numInv);
 
     return 0;
 }
